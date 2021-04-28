c66bc23753eb7336be89a4e8fc0a19b304245943
Lawrence Gorris <gorrislawrence.personal@gmail.com>
2021-04-28 10:57:00 +0200

    made game work; console log only => gonna show results on page, design page better, add other elements

--- script.js
+++ script.js
@@ -1,53 +1,80 @@
-/*   ---  SETUP - USER ---   */
+(function game(){
 
-let userChoice ;
+    let userChoice;
+    let computerChoice;
 
-const u_rock = document.querySelector('#u_rock').addEventListener('click', function(){
-    userChoice = "rock";
-    console.log(userChoice);
+    document.querySelector('#u_rock').addEventListener('click', function(){
+        userChoice = 'rock';
+    });
+    document.querySelector('#u_paper').addEventListener('click', function(){
+        userChoice = 'paper';
+    });
+    document.querySelector('#u_scissors').addEventListener('click', function(){
+        userChoice = 'scissors';
+    });
+    document.querySelector('#u_lizard').addEventListener('click', function(){
+        userChoice = 'lizard';
+    });
+    document.querySelector('#u_spock').addEventListener('click', function(){
+        userChoice = 'spock';
+    });
 
-});
-const u_paper = document.querySelector('#u_paper').addEventListener('click', function(){
-    userChoice = "paper";
-    console.log(userChoice);
-});
-const u_scissors = document.querySelector('#u_scissors').addEventListener('click', function(){
-    userChoice = "scissors";
-    console.log(userChoice);
-});
-const u_lizard = document.querySelector('#u_lizard').addEventListener('click', function(){
-    userChoice = "lizard";
-    console.log(userChoice);
-});
-const u_spock = document.querySelector('#u_spock').addEventListener('click', function(){
-    userChoice = "spock";
-    console.log(userChoice);
-});
+    document.querySelector('#comp_button').addEventListener('click', function(){
+        const calc_c = Math.floor(Math.random()*5);
+        if (calc_c == 0){
+            computerChoice = 'rock';
+            result();
+        }else if (calc_c == 1){
+            computerChoice = 'paper';
+            result();
+        }else if (calc_c == 2){
+            computerChoice = 'scissors';
+            result();
+        }else if (calc_c == 3){
+            computerChoice = 'lizard';
+            result();
+        }else {
+            computerChoice = 'spock';
+            result();
+        }
+    });
 
-/*   ---  SETUP - COMPUTER ---   */
-
-let computerChoice;
-
-const u_choice = document.querySelector('#comp_button').addEventListener('click', function(){
-    switch (Math.floor(Math.random()*5)){
-        case 0: 
-            computerChoice = "rock";
-            break;
-        case 1: 
-            computerChoice = "paper";
-            break;
-        case 2: 
-            computerChoice = "scissors";
-            break;
-        case 3: 
-            computerChoice = "lizard";
-            break;
-        case 4: 
-            computerChoice = "spock";
-            break;
+    function result(){
+        console.log(computerChoice);
+        if (userChoice === computerChoice){
+            console.log('oh my, it is a tie');
+        }else if (userChoice === 'rock'){
+            if (computerChoice === 'scissors' || computerChoice === 'lizard'){
+                console.log('you beat me')
+            }else {
+                console.log('ha, i won');
+            }
+        }else if (userChoice === 'paper'){
+            if (computerChoice === 'rock' || computerChoice === 'spock'){
+                console.log('you beat me')
+            }else {
+                console.log('ha, i won');
+            }
+        }else if (userChoice === 'scissors'){
+            if (computerChoice === 'paper' || computerChoice === 'lizard'){
+                console.log('you beat me')
+            }else {
+                console.log('ha, i won');
+            }
+        }else if (userChoice === 'lizard'){
+            if (computerChoice === 'paper' || computerChoice === 'spock'){
+                console.log('you beat me')
+            }else {
+                console.log('ha, i won');
+            }
+        }else if (userChoice === 'spock'){
+            if (computerChoice === 'rock' || computerChoice === 'scissors'){
+                console.log('you beat me')
+            }else {
+                console.log('ha, i won');
+            }
+        }
+        
     }
-
-    console.log(computerChoice)
-});
-
-/*   ---  CHECK FOR RESULTS ---   */
+    
+})();
